790766d02b2c85d4fe6d2f03e00e0abde07f8447
RayRay <2572614@edu.vaniercollege.qc.ca>
2026-02-27 11:14:47 -0500

    setup  101

---
+++ resona/src/components/LandingPage.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+<body class="app-container">
+
+</body>
+</template>
+
+<style lang="scss" scoped>
+ 
+</style>
---
+++ resona/src/views/HomepageView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import ContactsPage from '@/components/ContactsPage.vue';
+import LandingPage from '@/components/LandingPage.vue';
+import MembersPage from '@/components/MembersPage.vue';
+import PartnersPage from '@/components/PartnersPage.vue';
+import PortfolioPage from '@/components/PortfolioPage.vue';
+</script>
+
+<template>
+    <LandingPage />
+    <PortfolioPage />
+    <PartnersPage />
+    <MembersPage />
+ <ContactsPage />
+</template>
+
+<style lang="scss" scoped></style>
