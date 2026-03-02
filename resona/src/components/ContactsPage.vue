bdd9211bd0e6f2ba72814b882010ae767b0f8219
RayRay <2572614@edu.vaniercollege.qc.ca>
2026-03-02 01:43:42 -0500

    erm

--- resona/src/components/ContactsPage.vue
+++ resona/src/components/ContactsPage.vue
@@ -2,6 +2,51 @@
 
 <template>
 
+<body>
+    <header>
+        <h3 class="rubik">contact</h3>
+    </header>
+    <img src="../assets/television.png" alt id="tv"><video src=""></video>
+    <footer>
+        <a href=""><img src="../assets/ig-logo.png" alt id="ig"></a>
+        <div class="help">
+            <p class="lexend" id="title">Helpful Links</p>
+            <a href="" class="lexend">BPM festival</a>
+            <br>
+            <a href="" class="lexend">Presentation</a>
+        </div>
+        <div class="about">
+            <p class="lexend" id="title">About us</p>
+            <a href="https://582multi.media/" class="lexend">Contact us</a>
+            <br>
+            <a href="https://www.figma.com/design/h0LYGmI8g1IcsFRQilUHML/Capython-2026?node-id=0-1&t=8Ysswi7g8F5GYdgE-1" class="lexend">Figma</a>
+        </div>
+        <p class="lexend">&copy;2026 Resona</p>
+    </footer>
+</body>
+
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#tv {
+    grid-column: span 2;
+    text-align: center;
+    width: 300px;
+}
+
+#title {
+    font-weight: 600px;
+}
+
+footer {
+    grid-column: span 2;
+    text-align: center;
+    margin-top: 10px;
+    background-color: #8f0410;
+}
+
+a, p {
+    color: white;
+    text-decoration: none;
+}
+</style>
--- resona/src/components/MembersPage.vue
+++ resona/src/components/MembersPage.vue
@@ -7,13 +7,35 @@
         <h3 class="rubik">members</h3>
     </header>
     <div class="carousel-track">
-         
+        <button>
+          <p id="name" class="lexend">name</p>
+          <p id="description" class="lexend">description</p>
+        </button>
     </div>
     <div class="carousel-nav">
-
+        <button><</button>
+        <button>></button>
     </div>
   </body>
 
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+div {
+    grid-column: span 2;
+    text-align: center;
+    margin-top: 30px;
+}
+
+#name {
+  background-color: #260104;
+  border-radius: 20px;
+  color: white;
+}
+
+#description {
+  background-color: #8F0410;
+  border-radius: 20px;
+  color: white;
+}
+</style>
