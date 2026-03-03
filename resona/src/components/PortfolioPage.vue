771f2f95d3cd9a478ba5020a6e0c658cbb5cc4b8
RayRay <2572614@edu.vaniercollege.qc.ca>
2026-03-03 09:13:32 -0500

    portfolio

--- resona/src/components/PortfolioPage.vue
+++ resona/src/components/PortfolioPage.vue
@@ -7,11 +7,11 @@
         <h3 class="rubik">portfolio</h3>
     </header>
     <div>
-        <video width="300px"><source src="../assets/Motion Concept 1.mp4" type="video/mp4"></video>
+        <video src="../assets/Motion Concept 1.mp4" controls poster>motion concept 1</video>
         <figcaption class="lexend">description</figcaption>
     </div>
     <div>
-        <video width="300px"><source src="../assets/Motion Concept 2.mp4" type="video/mp4"></video>
+        <video src="../assets/Motion Concept 2.mp4" controls poster>motion concept 1</video>
         <figcaption class="lexend">description</figcaption>
     </div>
     <div>
@@ -42,7 +42,20 @@ figcaption {
 }
 
 video {
-    border-radius: 20px;
-    border: 5px solid #8F0410;
+  width: 300px;
+
+  background: #8F0410;
+  border: 5px solid transparent;
+  border-radius: 20px;
+  cursor: pointer;
+
+  background-image: 
+  linear-gradient(#8f0410, #8F0410),
+  radial-gradient(#D21436, #8C030E, #D21436);
+
+  background-origin: border-box;
+  background-clip: padding-box, border-box;
+
+  box-shadow: 0 0 12px rgba(143, 4, 16);
 }
 </style>
