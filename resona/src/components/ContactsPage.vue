659940dd1364f042345463f07da860fbd6815600
Kelly <2555156>
2026-03-03 09:13:31 -0500

    contacts kms

--- resona/src/components/ContactsPage.vue
+++ resona/src/components/ContactsPage.vue
@@ -2,38 +2,43 @@
 
 <template>
 
+
+
+    <div class="app-container">
     <header>
         <h3 class="rubik">contact</h3>
     </header>
+        <img id="tv" src="../assets/television.png" alt=""> <!-- <video src=""></video> /-->
+    </div>
 
-<div class="app-container">
-    <img id="tv" src="../assets/television.png" alt=""> <!-- <video src=""></video> /-->
-</div>
     <footer class="app-container">
         <a href=""><img src="../assets/ig-logo.png" alt id="ig"></a>
+        
         <div class="help">
             <p class="lexend" id="title">Helpful Links</p>
             <a href="" class="lexend">BPM festival</a>
             <br>
             <a href="" class="lexend">Presentation</a>
         </div>
+
         <div class="about">
             <p class="lexend" id="title">About us</p>
             <a href="https://582multi.media/" class="lexend">Contact us</a>
             <br>
             <a href="https://www.figma.com/design/h0LYGmI8g1IcsFRQilUHML/Capython-2026?node-id=0-1&t=8Ysswi7g8F5GYdgE-1" class="lexend">Figma</a>
         </div>
-        <img src="" alt id="logo">
+
         <p class="lexend" id="copy">&copy;2026 Resona</p>
+
+        <img src="../assets/wHITE VERSION.png" alt id="logo">
     </footer>
 
 </template>
 
 <style lang="scss" scoped>
 
-h3 {
-    // padding-right: 50px;
-    // padding-bottom: 30px;
+header {
+    grid-column: 3;
 }
 
 #tv {
@@ -47,6 +52,7 @@ h3 {
 #title {
     font-weight: bold;
 }
+
 a.lexend {
     font-weight: 250;
 }
@@ -70,23 +76,26 @@ a, p {
     margin-bottom: 20px;
 }
 
-#ig {
-    margin: 20px;
-}
-
 div .help {
-    margin: 20px;
+    // padding: 20px;
     text-align: right;
 }
 
 div .about {
-    margin: 20px;
+    // padding: 20px;
     text-align: right;
+}
+
+#logo {
+    width: 110px;
+    grid-column: 3;
+    justify-self: right;
 }
 .app-container {
     height: auto;
     grid-template-rows: 1fr;
     grid-template-columns: repeat(3, 1fr);
+    padding: 20px;
 }
 
 
