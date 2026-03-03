ca0cbe40b9f9f0caf144324c248055c4a18e5fad
Tyler <6233919@edu.vaniercollege.qc.ca>
2026-03-03 16:18:35 -0500

    i made a video

---
+++ bpm-vue/src/components/LatestPost.vue
@@ -0,0 +1,41 @@
+<script setup></script>
+
+<template>
+<main class="app-container">
+    <h1>Latest Post</h1>
+    <div class="img-container">
+        <video src="../assets/instapost.mp4" autoplay muted loop></video>
+    </div>
+</main>
+</template>
+
+<style scoped>
+.app-container {
+    h1 {
+        grid-column: span 2;
+        text-align: center;
+    }
+
+    .img-container {
+        display: grid;
+        grid-column: span 2;
+        grid-row: span 4;
+        margin: 0 auto;
+        height: 100%;
+
+        video {
+            width: 100%;
+            height: 600px;
+            object-fit: contain;
+        }
+    }
+}
+
+@media screen and (min-width: 1080px) {
+    .app-container {
+        h1, .img-container {
+            grid-column: span 5;
+        }
+    }
+}
+</style>
--- bpm-vue/src/views/HomeView.vue
+++ bpm-vue/src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AnnouncementBanner from '@/components/AnnouncementBanner.vue'
 import LandingPage from '@/components/LandingPage.vue'
+import LatestPost from '@/components/LatestPost.vue';
 import LineupPage from '@/components/LineupPage.vue'
 import WayfindPage from '@/components/WayfindPage.vue';
 </script>
@@ -10,4 +11,5 @@ import WayfindPage from '@/components/WayfindPage.vue';
   <LineupPage id="lineup" />
   <AnnouncementBanner id="announcement" />
   <WayfindPage id="wayfind" />
+  <LatestPost id="posts" />
 </template>
